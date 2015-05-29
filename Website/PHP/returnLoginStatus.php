50382ba7c5905e09bf76677a16a48602674d3d72
Elang89 <elang8899@gmail.com>
2015-05-29 13:14:47 -0600

    cambios

--- Website/PHP/returnLoginStatus.php
+++ Website/PHP/returnLoginStatus.php
@@ -3,9 +3,9 @@
 	session_start();
 	$userType;
 	
-	if(isset($_SESSION['usertype'])){
+	if(isset($_SESSION['usertype']) && $_SESSION['usertype'] == 1){
 		exit('1');
-	} else if(isset($_SESSION['usertype'])){
+	} else if(isset($_SESSION['usertype']) && $_SESSION['usertype'] == 0){
 		exit('0');		
 	} else {
 		exit('-1');
