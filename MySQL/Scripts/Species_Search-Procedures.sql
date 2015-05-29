51e1415c801e3de2bd2f09f611cd33c7f441077a
ricardo-cr23 <ricardo.cr023@gmail.com>
2015-05-29 02:24:10 -0600

    Update Species_Search-Procedures.sql

--- MySQL/Scripts/Species_Search-Procedures.sql
+++ MySQL/Scripts/Species_Search-Procedures.sql
@@ -56,3 +56,32 @@ BEGIN
         AND sub_order.FK_Order_Id = Orders.Order_Id 
         AND Orders.FK_Class_Id = Class.Class_Id;
 END 
+
+CREATE DEFINER=`DBadmin`@`localhost` PROCEDURE `find_bird`(
+        p_Specie_Name VARCHAR(45),
+        p_Size_Name VARCHAR(45),
+        p_Habitat_Name VARCHAR(45),
+        p_Beak_Name VARCHAR(45),
+        p_Color_Name VARCHAR(45),
+        p_Quantity VARCHAR(45),
+        p_Gender_Name VARCHAR(45),
+        p_Family_Name VARCHAR(45),
+        p_Sub_Order_Name VARCHAR(45),
+        p_Order_Name VARCHAR(45),
+        p_Class_Name VARCHAR(45)
+)
+BEGIN
+	SELECT Species_Id, Specie_Name, Size_Name, Habitat_Name, Beak_Name, Color_Name, Quantity, Gender_Name, Family_Name, Sub_Order_Name, Order_Name, Class_Name, Description
+	FROM species_found, specie, Size, Habitat, Beak_Type, Color, offspring_quantity, Gender, Family, Sub_Order, Orders, Class
+        WHERE p_Specie_Name = Specie_Name
+        OR p_Size_Name = Size_Name
+        OR p_Habitat_Name = Habitat_Name
+        OR p_Beak_Name = Beak_Name
+        OR p_Color_Name = Color_Name
+        OR p_Quantity = Quantity
+        OR p_Gender_Name = Gender_Name
+        OR p_Family_Name = Family_Name
+        OR p_Sub_Order_Name = Sub_Order_Name
+        OR p_Order_Name = Order_Name
+        OR p_Class_Name = Class_Name
+END
